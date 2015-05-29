e7f7cbea83aa9f05410ae2a160843038a0c0eb94
Jake Scott <jscott@addthis.com>
2015-05-29 09:28:23 -0400

    updated to latest fluxthis

--- src/actions/MyFirstActionCreator.es6.js
+++ src/actions/MyFirstActionCreator.es6.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const ActionCreator = require('fluxthis/src/ActionCreator');
-const ACTION_SOURCES = require('constants/ActionSources');
 const ACTION_TYPES = require('constants/ActionTypes');
 
 export default new ActionCreator({
--- src/constants/ActionSources.es6.js
+++
@@ -1,5 +0,0 @@
-const ConstantCollection = require('fluxthis/src/ConstantCollection');
-
-export default new ConstantCollection(
-	/* Your code here! */
-);
